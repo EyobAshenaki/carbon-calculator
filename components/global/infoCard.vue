d6698af9a4f616300667fe686000fdea15b3ae40
EyobAshenaki <ethioeyoba@gmail.com>
2023-02-11 15:47:04 +0300

    feat: change the info card and designers section
    - Styled and added new look for usability based on a redesign

--- components/global/infoCard.vue
+++ components/global/infoCard.vue
@@ -1,19 +1,37 @@
 <template>
   <div
-    class="h-80 flex flex-col justify-center items-center gap-10 rounded-2xl shadow-xl border p-5"
+    class="min-h-[20rem] flex flex-col justify-evenly items-center gap-3 rounded-2xl shadow-xl border p-6"
   >
     <img
       v-if="item.image"
-      class="w-28 h-28 mt-5"
+      class="w-20 h-20"
       :src="getImgSrc(item.image)"
       :alt="item.alt"
     />
+    <h3 class="text-lg font-bold">{{ item.name }}</h3>
+    <p v-if="item.title" class="font-normal text-sm -mt-2">{{ item.title }}</p>
+
     <p
-      class="text-custom-teal text-center"
-      :class="{ 'font-semibold': !hasImage }"
+      class="text-center"
+      :class="{ 'font-semibold text-custom-teal': !hasImage }"
     >
       {{ item.description }}
     </p>
+
+    <div
+      class="w-[90%] sm:w-full xl:w-[90%] flex flex-row lg:flex-col-reverse 2xl:flex-row items-center mt-2"
+      :class="[item.price ? 'justify-between' : 'justify-center']"
+    >
+      <span v-if="item.price" class="text-custom-teal font-bold text-lg"
+        >${{ item.price }} / hour</span
+      >
+      <button
+        class="bg-custom-teal text-white rounded-full px-5 py-2 mb-2"
+        @click="$emit('openModal', item)"
+      >
+        {{ item.price ? 'Book a call' : 'Open' }}
+      </button>
+    </div>
   </div>
 </template>
 
--- components/menuSection/Designers.vue
+++ components/menuSection/Designers.vue
@@ -1,16 +1,25 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <section class="w-full">
-    <h1 class="text-2xl font-bold mb-10">Order designer</h1>
+    <h1 class="text-2xl font-bold mb-10">Consulting</h1>
     <div class="flex flex-col lg:flex-row items-center gap-10">
-      <a
+      <designers-card
+        v-for="(item, idx) in consultingItems"
+        :key="idx"
+        :item="item"
+        class="w-[90%] sm:w-[70%] lg:w-1/3"
+        @openModal="openInquiryModal($event)"
+      />
+    </div>
+    <h1 class="text-2xl font-bold mb-10 mt-16">Order Designers</h1>
+    <div class="flex flex-col lg:flex-row items-center gap-10">
+      <designers-card
         v-for="(item, idx) in designersItems"
         :key="idx"
-        class="w-full sm:w-1/2 md:w-full lg:w-1/3 cursor-pointer"
-        @click="openInquiryModal(item)"
-      >
-        <designers-card :item="item" />
-      </a>
+        :item="item"
+        class="w-[90%] sm:w-[70%] lg:w-1/3"
+        @openModal="openInquiryModal($event)"
+      />
     </div>
     <inquiry-modal
       v-if="isInquiryModalOpen"
@@ -27,34 +36,69 @@ export default {
   components: { designersCard, InquiryModal },
   data() {
     return {
+      consultingItems: [
+        {
+          type: 'consultant',
+          name: 'Michael Testa',
+          title: 'CEO - GreenCheddar',
+          price: 80,
+          description:
+            'Do you want to hear my thought on your product or strategy?',
+          image: 'designers/consulting_image_1.svg',
+          alt: 'consulting image 1',
+        },
+        {
+          type: 'consultant',
+          name: 'Stefano C. Testa',
+          title: 'Sales - GreenCheddar',
+          price: 30,
+          description:
+            'Do you want to hear my thought on your product or strategy?',
+          image: 'designers/consulting_image_2.svg',
+          alt: 'consulting image 2',
+        },
+        {
+          type: 'consultant',
+          name: 'Someone',
+          title: 'Something - GreenCheddar',
+          price: 50,
+          description: 'Do you want to hear my thought on something?',
+          image: 'designers/consulting_image_3.svg',
+          alt: 'consulting image 3',
+        },
+      ],
       designersItems: [
         {
-          description: 'Small Modifications to Current Menu',
+          type: 'designer',
+          name: 'Modify menu',
+          description: 'Small modifications to current menu',
           image: 'designers/designers_image_1.svg',
-          alt: 'designers image 1',
+          alt: 'other designers image 1',
         },
         {
-          description: 'Design New Menu',
+          type: 'designer',
+          name: 'New menu',
+          description: 'Design new menus for your business',
           image: 'designers/designers_image_2.svg',
-          alt: 'designers image 2',
-        },
-        {
-          description:
-            'Display Climate Responsibility & Attract Climate Conscious Customers through Social Media',
-          image: 'designers/designers_image_3.svg',
-          alt: 'designers image 3',
+          alt: 'other designers image 2',
         },
       ],
       isInquiryModalOpen: false,
       selectedResponse: {
-        message: '',
         type: '',
+        name: '',
+        title: '',
+        price: '',
+        message: '',
       },
     }
   },
   methods: {
     openInquiryModal(item) {
-      this.selectedResponse.type = 'designers'
+      this.selectedResponse.type = item.type
+      this.selectedResponse.name = item.name
+      this.selectedResponse.title = item?.title
+      this.selectedResponse.price = item?.price
       this.selectedResponse.message = item.description
       if (!this.isInquiryModalOpen) this.isInquiryModalOpen = true
     },
